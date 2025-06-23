0428199df30d42416e609fafe8ffd70ce922471d
Philipp Spiess <hello@philippspiess.com>
2025-06-23 11:02:11 +0200

    Initial Commit

---
+++ WhatsTwo/WhatsTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsTwoApp.swift
+//  WhatsTwo
+//
+//  Created by Philipp on 23.06.25.
+//
+
+import SwiftUI
+
+@main
+struct WhatsTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
